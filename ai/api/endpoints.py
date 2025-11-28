9741c94d633414b8364f7f91a902c8bd8d1dfb97
felicon <eduardermakov769@gmail.com>
2025-11-28 22:55:48 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os      SOCODE
+import os      felicon
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
